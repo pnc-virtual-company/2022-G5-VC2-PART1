0eec06dc325cd933b96ebac04ed5066e515b50d4
somnak Kalan <kalansomnak12@gmail.com>
2022-08-16 16:55:26 +0700

    #7 [Navigation] Create Menu

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-  <Navigation/>
+    <h3 @click="isNavigation()">Student</h3>
+    <h3 @click="isNavAdmin()()">Admin</h3>
+    <div v-if="isStudent">
+      <StudentVIew />
+    </div>
+    <div v-if="isAmin">
+      <AdminVIew />
+    </div>
   </div>
 </template>
 
-<script setup>
-import Navigation from './navigation/NavigationBar.vue';
+<script>
+import StudentVIew from "./views/StudentView.vue";
+import AdminVIew from "./views/AminView.vue";
+export default {
+  data() {
+    return {
+      isStudent: false,
+      isAmin: false,
+    };
+  },
+  components: {
+    StudentVIew,
+    AdminVIew,
+  },
+  methods: {
+    isNavigation() {
+      this.isStudent = true;
+    },
+    isNavAdmin() {
+      this.isAmin = true;
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style >
 
 </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,20 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashBoard from "../components/DashBoard.vue"
+import NewRequest from "../components/NewRequest.vue"
+import CheckListStudent from "../components/CheckList.vue"
+import StudentList from "../components/StudentList.vue"
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'dashboard',
+    component: DashBoard
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/request',
+    name: 'request',
+    component: NewRequest
+  },
+  {
+    path: '/checkList',
+    name: 'checkList',
+    component: CheckListStudent
+  },
+  {
+    path: '/studentlist',
+    name: 'studentlist',
+    component: StudentList
+  },
 ]
 
 const router = createRouter({
