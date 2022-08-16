ffeeb7efbc04afd9dd1966abae3b72040578ed77
somnak Kalan <kalansomnak12@gmail.com>
2022-08-16 08:38:31 +0700

    #7 create navigation

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+  <Navigation/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script setup>
+import Navigation from './navigation/NavigationBar.vue';
+</script>
 
-nav {
-  padding: 30px;
-}
+<style lang="scss" scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
